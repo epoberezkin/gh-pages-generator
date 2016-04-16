e87a71fbae036e1e760a4864c8823a0ebab00b52
Evgeny Poberezkin <e.poberezkin@me.com>
2016-04-16 18:44:05 +0100

    create pages from markdown files

---
+++ index.js
@@ -0,0 +1,80 @@
+#! /usr/bin/env node
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var ajv = require('ajv')({ "useDefaults": true, "allErrors": true });
+var doT = require('dot');
+
+var config = JSON.parse(fs.readFileSync(process.argv[2] || 'site.json'));
+var schema = require('./site_config_schema.json');
+var valid = ajv.validate(schema, config);
+config.repository = escapePath(config.repository);
+
+if (!valid) {
+  console.log('site configuration file is invalid')
+  console.log(ajv.errorsText(ajv.errors, { separator: '\n' }));
+  process.exit(1);
+}
+
+var frontMatter, repoPattern;
+
+prepareTemplates();
+config.pages.forEach(createPage);
+
+
+function prepareTemplates() {
+  doT.templateSettings.strip = false;
+  frontMatter = fs.readFileSync(path.join(__dirname, 'front_matter.dot'));
+  frontMatter = doT.compile(frontMatter);
+}
+
+
+function createPage(page) {
+  readFile(page);
+  replaceLinks(page);
+  addFrontMatter(page);
+  writePage(page);
+}
+
+
+function readFile(page) {
+  var sourcePath = path.join(config.folders.source, page.file);
+  page.content = fs.readFileSync(sourcePath, 'utf8');
+}
+
+
+var rootRegex;
+function replaceLinks(page) {
+  config.pages.forEach(replaceLinksToPage);
+  if (config.folders.site) replaceLinksToRoot();
+
+  function replaceLinksToPage(toPage) {
+    if (!toPage.linkRegex) {
+      var pagePathPattern = config.repository + escapePath('/blob/master/' + toPage.file);
+      toPage.linkRegex = new RegExp(pagePathPattern, 'g');
+    }
+    page.content = page.content.replace(toPage.linkRegex, toPage.page + '.html');
+  }
+
+  function replaceLinksToRoot() {
+    rootRegex = rootRegex || new RegExp(config.repository + '(?:\\/)?#', 'g');
+    page.content = page.content.replace(rootRegex, '/' + config.folders.site + '#');
+  }
+}
+
+
+function addFrontMatter(page) {
+  page.content = frontMatter({ config: config, page: page }) + page.content;
+}
+
+
+function writePage(page) {
+  var targetPath = path.join(config.folders.target, page.page + '.md');
+  fs.writeFileSync(targetPath, page.content);
+}
+
+
+function escapePath(str) {
+  return str.replace(/(\/|\.)/g, '\\$1');
+}
